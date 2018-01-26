f7f5543220a32bd5549a08d5652a88eaef85a38e
marcelo.sousa <marcelo.sousa@assertivasolucoes.com.br>
2018-01-26 13:35:58 -0200

    eventos

--- sw.js
+++ sw.js
@@ -1,3 +1,13 @@
 self.addEventListener('install', function (event) {
     console.log('[Service Worker] Instalando Service Worker...', event);
 });
+
+self.addEventListener('activate', function (event) {
+    console.log('[Service Worker] Ativando Service Worker ...', event);
+    return self.clients.claim();
+});
+
+self.addEventListener('fetch', function (event) {
+    console.log('[Service Worker] Procurando algo...', event);
+    event.respondWith(fetch(event.request));
+});
