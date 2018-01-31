efa47207af9f309be3c073f38d256ad989c9015f
marcelo.sousa <marcelo.sousa@assertivasolucoes.com.br>
2018-01-31 09:01:19 -0200

    banner de instalacao

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,6 @@
+//referencia do prompt
+var bannerInstall;
+
 if ('serviceWorker' in navigator){
     navigator.serviceWorker
         .register('sw.js')
@@ -5,3 +8,34 @@ if ('serviceWorker' in navigator){
             console.log('Service worker está registrado!');
         });
 }
+
+//evento disparado para instalar a aplicacao
+window.addEventListener('beforeinstallprompt', function(event) {
+    console.log('beforeinstallprompt disparado...');
+    //prevenindo a instalacao e guardando a referencia
+    event.preventDefault();
+    bannerInstall = event;
+    return false;
+  });
+
+//  Controlando o banner de instalacao 
+var btnBannerInstall = document.querySelector('#enable-banner-install');
+
+function showBannerInstall(){
+    if(bannerInstall){
+        //exibindo o banner
+        bannerInstall.prompt();
+        //verificando a escolha do usuario
+        bannerInstall.userChoice.then(function(choiceResult) {
+            if (choiceResult.outcome === 'dismissed') {
+                console.log('Usuario cancelou a instalacao');
+              } else {
+                console.log('Usuario aceitou a instalacao');
+              }
+        });
+    }
+
+    bannerInstall = null;
+}
+//clique do botao
+btnBannerInstall.addEventListener('click', showBannerInstall);
